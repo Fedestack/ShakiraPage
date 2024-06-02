a746355954f19bcc2d1598d0fc10b3c9381961f5
Fede <fedenegreira@gmail.com>
2024-06-01 21:01:16 -0300

    validacion inputs desde js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function abrirEnOtraPestana() {
     window.open('https://www.google.com.ar', '_blank');
 }
+
+//valido los inputs del formulario desde js
+const inputs = document.querySelectorAll(".form_input_textarea");
+
+inputs.forEach(input => {
+    input.required = true;
+});
+
+
